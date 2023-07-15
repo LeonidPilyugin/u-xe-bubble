16da649a30aaa292085d4d1f37f999b79be39d80
Leonid Pilyugin <l.pilyugin04@gmail.com>
2023-07-15 13:52:48 +0300

    Exception handling

--- src/kernel.py
+++ src/kernel.py
@@ -2,6 +2,12 @@ import os
 from state import State
 from plugin import Plugin
 
+class KernelException(Exception):
+    pass
+
+class InvalidConfig(KernelException):
+    pass
+
 
 class Kernel:
     
@@ -9,12 +15,18 @@ class Kernel:
         self.state = State()
         self.state.load(config)
         self.plugins = {}
-        self.sequence = self.state["sequence"]
+        try:
+            self.sequence = self.state["sequence"]
+        except KeyError:
+            raise InvalidConfig("Cannot find sequence")
     
     
     def load(self):
-        for plugin_name in self.state["plugins"]:
-            self.plugins[plugin_name] = Plugin(os.path.join(os.path.dirname(__file__), "../plugins", plugin_name))
+        try:
+            for plugin_name in self.state["plugins"]:
+                self.plugins[plugin_name] = Plugin(os.path.join(os.path.dirname(__file__), "../plugins", plugin_name))
+        except KeyError:
+            raise InvalidConfig("Cannot find plugins")
     
     
     def run(self):
